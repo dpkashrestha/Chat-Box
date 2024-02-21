0afd3a943d1839f4b5e98b77cb68a6dcbeb8c231
Alex Majorowicz <alexmajors0613@gmail.com>
2024-02-21 01:05:06 -0600

    add Chat typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,35 @@
 const typeDefs = `
-type User {
-    _id: ID
-    firstName: String
-    lastName: String
-    email: String
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    chats: [Chat]
+  }
+
+  type Chat {
+    _id: ID!
+    chatName: String!
+    users: [User]!
+    messages: [Message]
   }
 
   type Query {
     me: User
+    users: [User]
+    getChats: [Chat]
   }
 
   type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    createChat(chatName: String!, users: [User]!): Chat
+    getChats: [Chat]!
   }
-  
-    type Auth {
-        token: ID!
-        user: User
-    }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
