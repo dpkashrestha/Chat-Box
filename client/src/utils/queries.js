88e2e02f0a93b0d3ea09c238543ff9cb8c1cf544
dpkashrestha <dpkashrestha7@gmail.com>
2024-02-22 22:10:00 -0600

    Minor fix

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -11,15 +11,17 @@ export const QUERY_USERS = gql`
 `;
 
 export const QUERY_CHATS = gql`
-chats {
-  _id
-  chatName
-  users {
-    _id
-    username
-    email
+  query {
+    chats {
+      _id
+      chatName
+      users {
+        _id
+        username
+        email
+      }
+    }
   }
-}
 `;
 
 export const QUERY_ME = gql`
@@ -33,21 +35,23 @@ export const QUERY_ME = gql`
 `;
 
 export const QUERY_MESSAGES = gql`
-  query messages(chatId: $chatId) {
-    _id
-    content
-    sender {
+  query {
+    messages(chatId: $chatId) {
       _id
-      username
-      email
-    }
-    chat {
-      _id
-      chatName
-      users {
+      content
+      sender {
         _id
         username
         email
+      }
+      chat {
+        _id
+        chatName
+        users {
+          _id
+          username
+          email
+        }
       }
     }
   }
