b1c21a6e2ed976ce21a119de465fb0278eb472c3
Alex Majorowicz <alexmajors0613@gmail.com>
2024-02-23 04:43:32 -0600

    add chat and message seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,13 +1,17 @@
 const db = require("../config/connection");
-const { User } = require("../models");
+const { User, Chat, Message } = require("../models");
 const userSeeds = require("./userSeeds.json");
 const cleanDB = require("./cleanDB");
 
 db.once("open", async () => {
   try {
     await cleanDB("User", "users");
+    await cleanDB("Chat", "chats");
+    await cleanDB("Message", "messages");
 
     await User.create(userSeeds);
+    await Chat.create(chatSeeds);
+    await Message.create(messageSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
