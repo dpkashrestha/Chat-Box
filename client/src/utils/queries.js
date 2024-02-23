8035c7952b68e8bbd1a3e0e7d8a2fe872c8dbca9
dpkashrestha <dpkashrestha7@gmail.com>
2024-02-22 21:34:45 -0600

    Added queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -9,3 +9,46 @@ export const QUERY_USERS = gql`
     }
   }
 `;
+
+export const QUERY_CHATS = gql`
+chats {
+  _id
+  chatName
+  users {
+    _id
+    username
+    email
+  }
+}
+`;
+
+export const QUERY_ME = gql`
+  query {
+    me {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_MESSAGES = gql`
+  query messages(chatId: $chatId) {
+    _id
+    content
+    sender {
+      _id
+      username
+      email
+    }
+    chat {
+      _id
+      chatName
+      users {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
