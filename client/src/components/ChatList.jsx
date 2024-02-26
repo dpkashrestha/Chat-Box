25413933011fb2b9f7269deeacde000650db550f
dpkashrestha <dpkashrestha7@gmail.com>
2024-02-26 16:39:02 -0600

    Aded individual users in the sidebar

--- client/src/components/ChatList.jsx
+++ client/src/components/ChatList.jsx
@@ -11,13 +11,14 @@ import {
   AvatarGroup,
 } from "@chatscope/chat-ui-kit-react";
 import CreateModal from "./CreateChat";
+import Accordion from "react-bootstrap/Accordion";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOutAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
-import { QUERY_CHATS } from "../utils/queries";
+import { QUERY_CHATS, QUERY_USERS } from "../utils/queries";
 import Auth from "../utils/auth";
 
 const ChatList = ({ onClickCallback }) => {
@@ -25,8 +26,13 @@ const ChatList = ({ onClickCallback }) => {
   const [search, setSearch] = useState("");
   const [newGroup, setNewGroup] = useState(true);
   const [selectedChatId, setSelectedChatId] = useState(null);
-  const { loading, data } = useQuery(QUERY_CHATS, {
+  const { loading: loadingChatData, data: chatData } = useQuery(QUERY_CHATS, {
     variables: { chatName: search },
+    onCompleted: (data) => {
+      console.log(data);
+    },
+  });
+  const { loading: loadingUserData, data: userData } = useQuery(QUERY_USERS, {
     onCompleted: (data) => {
       console.log(data);
     },
@@ -68,55 +74,87 @@ const ChatList = ({ onClickCallback }) => {
         </Button>
       </CreateModal>
 
-      {loading ? (
+      {loadingChatData || loadingUserData ? (
         <Loader style={{ justifyContent: "center" }}>Loading</Loader>
       ) : (
-        <ConversationList>
-          {data.allChats.map((chat) => {
-            const otherUsers = getOtherUsers(chat.users);
-            const lastMessage = chat.lastMessage;
-            return (
-              <Conversation
-                key={chat._id}
-                name={chat.chatName}
-                lastSenderName={
-                  lastMessage ? lastMessage.sender.username : null
-                }
-                info={lastMessage ? lastMessage.content : "No messages yet"}
-                onClick={() => {
-                  setSelectedChatId(chat._id);
-                  onClickCallback(chat);
-                }}
-                active={selectedChatId === chat._id}
-              >
-                {otherUsers.length > 1 ? (
-                  <AvatarGroup size="sm">
-                    {otherUsers.slice(0, 4).map((user) => {
-                      return (
+        <>
+          <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Chats</Accordion.Header>
+              <Accordion.Body>
+                <ConversationList>
+                  {chatData.allChats.map((chat) => {
+                    const otherUsers = getOtherUsers(chat.users);
+                    const lastMessage = chat.lastMessage;
+                    return (
+                      <Conversation
+                        key={chat._id}
+                        name={chat.chatName}
+                        lastSenderName={
+                          lastMessage ? lastMessage.sender.username : null
+                        }
+                        info={
+                          lastMessage ? lastMessage.content : "No messages yet"
+                        }
+                        onClick={() => {
+                          setSelectedChatId(chat._id);
+                          onClickCallback(chat);
+                        }}
+                        active={selectedChatId === chat._id}
+                      >
+                        {otherUsers.length > 1 ? (
+                          <AvatarGroup size="sm">
+                            {otherUsers.slice(0, 4).map((user) => {
+                              return (
+                                <Avatar
+                                  key={user._id}
+                                  name={user.username}
+                                  src={`data:image/svg+xml;base64,${user.avatar}`}
+                                />
+                              );
+                            })}
+                          </AvatarGroup>
+                        ) : (
+                          <Avatar
+                            key={otherUsers[0]._id}
+                            name={otherUsers[0].username}
+                            src={`data:image/svg+xml;base64,${otherUsers[0].avatar}`}
+                          />
+                        )}
+                      </Conversation>
+                    );
+                  })}
+                </ConversationList>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Users</Accordion.Header>
+              <Accordion.Body>
+                <ConversationList>
+                  {userData.users.map((user) => {
+                    return (
+                      <Conversation
+                        key={user._id}
+                        name={user.username}
+                        onClick={() => {
+                          // setSelectedChatId(chat._id);
+                          // onClickCallback(chat);
+                        }}
+                        // active={selectedChatId === chat._id}
+                      >
                         <Avatar
                           key={user._id}
                           name={user.username}
                           src={`data:image/svg+xml;base64,${user.avatar}`}
                         />
-                      );
-                    })}
-                  </AvatarGroup>
-                ) : (
-                  <Avatar
-                    key={otherUsers[0]._id}
-                    name={otherUsers[0].username}
-                    src={`data:image/svg+xml;base64,${otherUsers[0].avatar}`}
-                  />
-                )}
-
-                {/* <Avatar
-                  name={lastMessage.sender.username}
-                  src={`data:image/svg+xml;base64,${lastMessage.sender.avatar}`}
-                /> */}
-              </Conversation>
-            );
-          })}
-        </ConversationList>
+                      </Conversation>
+                    );
+                  })}
+                </ConversationList>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </>
       )}
 
       <Button
