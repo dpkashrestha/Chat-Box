ddd663ae4c765c5acf63cedd8270994585d8ccef
Alex Majorowicz <alexmajors0613@gmail.com>
2024-02-27 19:33:51 -0600

    troubleshooting

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,6 +4,7 @@ import SignUpForm from "../components/SignupForm";
 import { LandingHeader } from "../components/LandingHeader";
 import { LandingFooter } from "../components/LandingFooter";
 import Auth from "../utils/auth";
+import { Button } from "react-bootstrap";
 
 const LandingPage = () => {
   const [showSignup, setShowSignup] = useState(false);
@@ -19,6 +20,26 @@ const LandingPage = () => {
   if (Auth.loggedIn()) {
     window.location.assign("/chat");
   }
+
+  const installBtn = document.getElementById("installBtn");
+  const textHeader = document.getElementById("textHeader");
+
+  window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    installBtn.style.visibility = "visible";
+    textHeader.textContent = "Click the button to install!";
+
+    installBtn.addEventListener("click", () => {
+      event.prompt();
+      installBtn.setAttribute("disabled", true);
+      installBtn.textContent = "Installed!";
+    });
+  });
+
+  window.addEventListener("appinstalled", (event) => {
+    textHeader.textContent = "Successfully installed!";
+    console.log("👍", "appinstalled", event);
+  });
 
   return (
     <>
@@ -52,6 +73,10 @@ const LandingPage = () => {
             </p>
           </>
         )}
+        <div id="textHeader">Click the button to install!</div>
+        <Button id="installBtn" variant="dark">
+          Install
+        </Button>
 
         <LandingFooter />
       </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,38 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
 
-  manifest: {
-    name: 'Chat-box',
-    short_name: 'Chat-box',
-    description: 'My messaging app',
-    theme_color: '#ffffff',
-    icons: [
-      {
-        src: 'pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'any'
+      manifest: {
+        name: "Chat-box",
+        short_name: "Chat-box",
+        description: "My messaging app",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "./chat-logo1.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
       },
-      {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any'
-      },
-      {
-        src: './public/chat-logo1.png',
-        sizes: '144x144',
-        type: 'image/png',
-        purpose: 'any',
-      }
-    ]
-  }
-})],
+    }),
+  ],
   server: {
     port: 3000,
     open: false,
