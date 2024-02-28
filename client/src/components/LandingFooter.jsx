7004b6e3d7bf129753936355c6bc56c8900d557c
dpkashrestha <dpkashrestha7@gmail.com>
2024-02-27 20:42:09 -0600

    footer style

--- client/src/components/LandingFooter.jsx
+++ client/src/components/LandingFooter.jsx
@@ -3,7 +3,18 @@ import React from "react";
 export function LandingFooter(props) {
   return (
     <>
-      <div className="landing-footer">
+      <div className="landing-footer-left">
+        <a
+          href="https://buy.stripe.com/test_7sIaIqaxafRx4r66oo"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="ml-2"
+        >
+          {" "}
+          ❤️ Support the developers
+        </a>
+      </div>
+      <div className="landing-footer-right">
         <span>UI components provided by:</span>
         <a
           href="https://chatscope.io"
@@ -13,14 +24,6 @@ export function LandingFooter(props) {
         >
           chatscope
         </a>
-
-      <a 
-      href="https://buy.stripe.com/test_7sIaIqaxafRx4r66oo"
-      rel="noopener noreferrer"
-      target="_blank"
-      className="ml-2"
-      >Support the developers</a>
-       
       </div>
     </>
   );
